b2bfcf4e41a4015fc662436762464f087dc8b39e
Adarsh <adarshniharika20@gmail.com>
2026-04-07 23:24:57 +0530

    uc1 created

--- Employment_Wage/Employment_Wage/Program.cs
+++ Employment_Wage/Employment_Wage/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+class EmployeeWage
+{
+    private string companyName;
+
+    public EmployeeWage(string companyName)
+    {
+        this.companyName = companyName;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Company: " + companyName);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EmployeeWage emp = new EmployeeWage("TCS");
+        emp.Display();
+    }
+}
