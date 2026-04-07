a3181ec1f60b0f730856fff7fd573e35f3ae8f84
Adarsh <adarshniharika20@gmail.com>
2026-04-07 23:49:19 +0530

    uc8 created

--- Employment_Wage/Employment_Wage/Program.cs
+++ Employment_Wage/Employment_Wage/Program.cs
@@ -19,7 +19,7 @@ class EmployeeWage
         return (empCheck == 1) ? 8 : (empCheck == 2 ? 4 : 0);
     }
 
-    public void ComputeWageWithLimit()
+    public void ComputeWage()
     {
         int totalHours = 0;
         int totalDays = 0;
@@ -30,9 +30,7 @@ class EmployeeWage
             totalHours += GetDailyHours();
         }
 
-        int totalSalary = totalHours * wagePerHour;
-
-        Console.WriteLine("Total Wage: " + totalSalary);
+        Console.WriteLine(companyName + " Wage: " + totalHours * wagePerHour);
     }
 }
 
@@ -41,9 +39,9 @@ class Program
     static void Main(string[] args)
     {
         EmployeeWage emp = new EmployeeWage("TCS", 20);
-        emp.ComputeWageWithLimit();
-
         EmployeeWage emp1 = new EmployeeWage("Maatrum", 40);
-        emp1.ComputeWageWithLimit();
+
+        emp.ComputeWage();
+        emp1.ComputeWage();
     }
 }
