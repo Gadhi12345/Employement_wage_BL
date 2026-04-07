562989eed39ac5e6b4436e3359174f08ec750739
Adarsh <adarshniharika20@gmail.com>
2026-04-07 23:52:29 +0530

    uc9 created

--- Employment_Wage/Employment_Wage/Program.cs
+++ Employment_Wage/Employment_Wage/Program.cs
@@ -4,22 +4,42 @@ class EmployeeWage
 {
     private string companyName;
     private int wagePerHour;
+    private int workingDays;
 
-    public EmployeeWage(string companyName, int wagePerHour)
+    public EmployeeWage(string companyName, int wagePerHour, int workingDays)
     {
         this.companyName = companyName;
         this.wagePerHour = wagePerHour;
+        this.workingDays = workingDays;
     }
 
-    public int GetDailyHours()
+    private int GetDailyHours()
     {
         Random rand = new Random();
         int empCheck = rand.Next(0, 3);
 
-        return (empCheck == 1) ? 8 : (empCheck == 2 ? 4 : 0);
+        switch (empCheck)
+        {
+            case 1: return 8;
+            case 2: return 4;
+            default: return 0;
+        }
     }
 
-    public void ComputeWage()
+    public void ComputeEmployeeWage()
+    {
+        int totalHours = 0;
+
+        for (int i = 0; i < workingDays; i++)
+        {
+            totalHours += GetDailyHours();
+        }
+
+        Console.WriteLine("Company: " + companyName);
+        Console.WriteLine("Total Wage: " + totalHours * wagePerHour);
+    }
+
+    public void ComputeWageWithLimit()
     {
         int totalHours = 0;
         int totalDays = 0;
@@ -30,7 +50,8 @@ class EmployeeWage
             totalHours += GetDailyHours();
         }
 
-        Console.WriteLine(companyName + " Wage: " + totalHours * wagePerHour);
+        Console.WriteLine("Company: " + companyName);
+        Console.WriteLine("Total Wage (Limit): " + totalHours * wagePerHour);
     }
 }
 
@@ -38,10 +59,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        EmployeeWage emp = new EmployeeWage("TCS", 20);
-        EmployeeWage emp1 = new EmployeeWage("Maatrum", 40);
+        EmployeeWage emp = new EmployeeWage("TCS", 20, 10);
+        emp.ComputeEmployeeWage();
+        emp.ComputeWageWithLimit();
 
-        emp.ComputeWage();
-        emp1.ComputeWage();
+        Console.WriteLine();
+
+        EmployeeWage emp1 = new EmployeeWage("Maatrum", 40, 10);
+        emp1.ComputeEmployeeWage();
+        emp1.ComputeWageWithLimit();
     }
 }
